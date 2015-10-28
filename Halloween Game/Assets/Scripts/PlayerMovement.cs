26682182fe8139f7ae79ebd6b2045e5857c10485
Vincent Vu <vincent.vu@hva.nl>
2015-10-28 11:25:02 +0100

    Changed shit and made a player movement script

---
+++ Halloween Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    private Vector2 _input, _movement;
+    float speed = 5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _movement = _input.normalized * speed * Time.deltaTime;
+        transform.Translate(_movement);
+    }
+}
