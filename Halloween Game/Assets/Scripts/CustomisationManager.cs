415582d7e52efd04fac84dc4a54f2ea48eaf52b1
Mike Veldsink <veldsinkie.mv@gmail.com>
2015-10-29 01:48:54 +0100

    Customisation, player grafics (weird word), and team fortress implementation (hats)

---
+++ Halloween Game/Assets/Scripts/CustomisationManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomisationManager : MonoBehaviour
+{
+    [SerializeField]
+    SpriteRenderer Head;
+    [SerializeField]
+    SpriteRenderer[] Bodyparts;
+
+    [SerializeField]
+    Color PreferredColor;
+    [SerializeField]
+    int PreferredHat;
+
+    [SerializeField]
+    Sprite[] Hats;
+
+    void Start()
+    {
+        for(int i = 0; i < Bodyparts.Length; i++)
+        {
+            Bodyparts[i].color = PreferredColor;
+        }
+        Head.sprite = Hats[PreferredHat];
+        enabled = false;
+    }
+}
