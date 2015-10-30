24ef8aa310913a765c838c612d645ab81a2fcbb0
Mike Veldsink <veldsinkie.mv@gmail.com>
2015-10-30 15:57:41 +0100

    pulling.

--- Halloween Game/Assets/Scripts/PumpkinAI.cs
+++ Halloween Game/Assets/Scripts/PumpkinAI.cs
@@ -45,7 +45,6 @@ public class PumpkinAI : EnemyBaseAI
         float output = max - Vector3.Distance(CurrentTarget.transform.position, transform.position);
         if (output < speed) return speed;
         else if (output > max) return max;
-        Debug.Log(output);
         return output;
     }
 }
