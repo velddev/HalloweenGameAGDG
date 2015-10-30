8cad28c697e57874afe79dcc6b49fe79b5ba328c
Vincent Vu <vincent.vu@hva.nl>
2015-10-30 15:21:37 +0100

    EnemySpawner array to add more enemies and spawn random shit

--- Halloween Game/Assets/Scripts/SpawnManager.cs
+++ Halloween Game/Assets/Scripts/SpawnManager.cs
@@ -4,23 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class SpawnManager : MonoBehaviour {
-   public GameObject item;
+   //public GameObject item;
     
    public GameObject[] _spawners;
-   private Vector3 _spawnPos;
-   bool canSpawn;
-   private int _spawnDelay, _timeDelay = 10, _lastIncreasedTime;
+   public GameObject[] Enemies;
 
+   private Vector3 _spawnPos;
+   private bool canSpawn;
+   private int _spawnDelay, _timeDelay, _lastIncreasedTime;
 
    void Awake()
    {
        _spawners = GameObject.FindGameObjectsWithTag("Respawn").ToArray();
-       _spawnDelay = 5; canSpawn = true;
+       _spawnDelay = 5; canSpawn = true; _timeDelay = 30;
    }
-	// Use this for initialization
 
-	
-	// Update is called once per frame
     void Update()
     {
         if (canSpawn) { StartCoroutine(SpawnMob(_spawnDelay)); }
@@ -30,19 +28,14 @@ public class SpawnManager : MonoBehaviour {
         {
             _lastIncreasedTime = (int)Time.time;
             _spawnDelay -= 1;
-        }
-
-        
-        
-      
-          
+        }          
     }
 
     IEnumerator SpawnMob(int timeBetweenEnemies)
     {
         canSpawn = false;
         _spawnPos = _spawners[Random.Range(0, _spawners.Length)].transform.position;
-        Instantiate(item, _spawnPos, transform.rotation);
+        Instantiate(Enemies[Random.Range(0, Enemies.Length)], _spawnPos, transform.rotation);
         yield return new WaitForSeconds(timeBetweenEnemies);
         canSpawn = true;
     }
