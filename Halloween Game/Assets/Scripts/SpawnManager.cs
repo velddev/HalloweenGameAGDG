0f5d65b5666c84491e26cce6cbe5ca9aaac7764c
Vincent Vu <vincent.vu@hva.nl>
2015-10-30 15:12:22 +0100

    Spawnincrease

--- Halloween Game/Assets/Scripts/SpawnManager.cs
+++ Halloween Game/Assets/Scripts/SpawnManager.cs
@@ -8,22 +8,42 @@ public class SpawnManager : MonoBehaviour {
     
    public GameObject[] _spawners;
    private Vector3 _spawnPos;
-   
+   bool canSpawn;
+   private int _spawnDelay, _timeDelay = 10, _lastIncreasedTime;
 
+
+   void Awake()
+   {
+       _spawners = GameObject.FindGameObjectsWithTag("Respawn").ToArray();
+       _spawnDelay = 5; canSpawn = true;
+   }
 	// Use this for initialization
-	void Start () {
-        _spawners  = GameObject.FindGameObjectsWithTag("Respawn").ToArray();
-        
-	}
+
 	
 	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
+        if (canSpawn) { StartCoroutine(SpawnMob(_spawnDelay)); }
+        if (_spawnDelay <= 2) { _spawnDelay = 2; }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Time.time > _timeDelay + _lastIncreasedTime)
         {
-            _spawnPos = _spawners[Random.Range(0, _spawners.Length)].transform.position;
-            Instantiate(item, _spawnPos, transform.rotation);
+            _lastIncreasedTime = (int)Time.time;
+            _spawnDelay -= 1;
         }
-        }
-	
+
+        
+        
+      
+          
+    }
+
+    IEnumerator SpawnMob(int timeBetweenEnemies)
+    {
+        canSpawn = false;
+        _spawnPos = _spawners[Random.Range(0, _spawners.Length)].transform.position;
+        Instantiate(item, _spawnPos, transform.rotation);
+        yield return new WaitForSeconds(timeBetweenEnemies);
+        canSpawn = true;
+    }
 }
