cf1850d8bf0241406965f088a642874be5befab3
KiesEenNaam <vincent_vdn@msn.com>
2015-10-30 20:31:03 +0100

    Weapons

---
+++ Halloween Game/Assets/Scripts/WeaponGlock.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponGlock : WeaponBase {
+
+    public override void OnMove()
+    {
+        transform.Translate(Vector2.up * Speed * Time.deltaTime);
+    }
+
+    public override void OnDie()
+    {
+        base.OnDie();
+    }
+}
